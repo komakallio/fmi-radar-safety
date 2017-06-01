62acbaa559f658b9508b1c1abd785a73ea00c822
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2017-06-02 00:43:06 +0300

    Remove owslib dependency.

--- radarsafety.py
+++ radarsafety.py
@@ -2,8 +2,9 @@
 
 import configparser
 import os
+import urllib.parse
+import urllib.request
 import time_utils
-from owslib.wms import WebMapService
 
 # Komakallio location in EPSG:3067 coordinates
 KOMAKALLIO_EPSG3067 = (355121.064967, 6673513.77179)
@@ -17,7 +18,14 @@ BOUNDING_BOX = (KOMAKALLIO_EPSG3067[0] - BOUNDING_BOX_SIZE / 2, KOMAKALLIO_EPSG3
                 KOMAKALLIO_EPSG3067[0] + BOUNDING_BOX_SIZE / 2, KOMAKALLIO_EPSG3067[1] + BOUNDING_BOX_SIZE / 2)
 
 # Image size in pixels
-IMAGE_SIZE = 2 * (int(BOUNDING_BOX_SIZE / METERS_PER_PIXEL),)
+IMAGE_SIZE = int(BOUNDING_BOX_SIZE / METERS_PER_PIXEL)
+
+
+def tuple_to_url_string(tuple_object):
+    output_string = ''
+    for item in tuple_object:
+        output_string += str(item) + ','
+    return output_string[:-1]
 
 
 def main():
@@ -39,18 +47,25 @@ def main():
     print('Fetching radar image for {}'.format(radar_time_string))
 
     # Fetch radar image
-    base_url = 'http://wms.fmi.fi/fmi-apikey/' + api_key + '/geoserver/Radar/wms'
-    wms = WebMapService(base_url, version='1.3.0')
-    image_data = wms.getmap(layers=['suomi_rr_eureffin'],
-                            srs='EPSG:3067',
-                            bbox=BOUNDING_BOX,
-                            size=IMAGE_SIZE,
-                            time=radar_time_string,
-                            styles=['Radar rr'],
-                            #styles=['raster'],
-                            format='image/png')
-    with open('radar.png', 'wb') as outfile:
-        outfile.write(image_data.read())
+    base_url = 'http://wms.fmi.fi/fmi-apikey/' + api_key + '/geoserver/Radar/wms?'
+    wms_params = {
+        'service': 'WMS',
+        'version': '1.3.0',
+        'request': 'GetMap',
+        'layers': 'suomi_rr_eureffin',
+        'crs': 'EPSG:3067',
+        'bbox': tuple_to_url_string(BOUNDING_BOX),
+        'styles': 'Radar rr',
+        'width': IMAGE_SIZE,
+        'height': IMAGE_SIZE,
+        'format': 'image/png',
+        'time': radar_time_string
+    }
+
+    complete_url = base_url + urllib.parse.urlencode(wms_params)
+    with urllib.request.urlopen(complete_url) as response:
+        with open('radar.png', 'wb') as outfile:
+            outfile.write(response.read())
 
 
 if __name__ == '__main__':
